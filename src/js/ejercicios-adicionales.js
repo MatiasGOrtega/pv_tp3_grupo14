15d97435fba1305e608d9c368ca69b318bf1f19a
Cari UwU <165955604+CariLucky@users.noreply.github.com>
2025-05-04 23:27:09 -0300

    punto adicional 2 (#12)

--- src/js/ejercicios-adicionales.js
+++ src/js/ejercicios-adicionales.js
@@ -1,4 +1,4 @@
-import { mostrarlista } from "./funciones.js";
+import { mostrarlista, filtrarProductos} from "./funciones.js";
 let productos = 
 [
     { descripcion: "Auriculares", precio: 49000 },
@@ -8,4 +8,24 @@ let productos =
     { descripcion: "Webcam", precio: 22000 }
   ];
   
+
+// 1 - Mostrar en consola cada producto en el formato: "Producto: [descripción] - Precio: 
+// $[precio]", usando forEach. 
 mostrarlista(productos);
+
+// 2 - Crear un nuevo array con los productos cuyo precio sea mayor a $20, usando filter. 
+filtrarProductos(productos);
+
+
+// 3 - Crear un array con los productos, pero con el precio con IVA incluido (21%), usando 
+// map. 
+
+
+// 4 - Ordenar el array original de productos por precio de menor a mayor, usando sort. 
+
+
+// 5 - Agregar un nuevo producto al final del array (por ejemplo, { descripcion: "Parlante 
+// Bluetooth", precio: 59000.90 }). 
+
+
+// 6 - Eliminar el producto con el precio más bajo del array. Mostrar cómo queda el array. 
--- src/js/funciones.js
+++ src/js/funciones.js
@@ -4,3 +4,12 @@ export const  mostrarlista =(productos)=>{
     console.log(`Producto: ${producto.descripcion} - Precio: $${producto.precio}`);
   });
 }
+
+
+export const filtrarProductos = (productos) => {
+  const productosMayoresA20k = productos.filter(producto => producto.precio > 20000);
+    console.log("\n2 - Productos con precio mayor a $20.000:");
+    console.log(productosMayoresA20k);
+}
+
+
