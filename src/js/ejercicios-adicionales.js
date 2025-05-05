218a42e69e6c643e2f7eba96264ab73cbdeb5d40
safaqui00 <safaqui00@gmail.com>
2025-05-04 23:12:19 -0300

    punto adicional 1 (#11)

--- src/js/ejercicios-adicionales.js
+++ src/js/ejercicios-adicionales.js
@@ -1,4 +1,6 @@
-let productos = [
+import { mostrarlista } from "./funciones.js";
+let productos = 
+[
     { descripcion: "Auriculares", precio: 49000 },
     { descripcion: "Teclado", precio: 30000 },
     { descripcion: "Mouse", precio: 15000 },
@@ -6,12 +8,4 @@ let productos = [
     { descripcion: "Webcam", precio: 22000 }
   ];
   
-  console.log("1 - Lista de productos:");
-  productos.forEach(producto => {
-    console.log(`Producto: ${producto.descripcion} - Precio: $${producto.precio}`);
-  });
-
-  const productosMayoresA20k = productos.filter(producto => producto.precio > 20000);
-  console.log("\n2 - Productos con precio mayor a $20.000:");
-  console.log(productosMayoresA20k);
-  
+mostrarlista(productos);
